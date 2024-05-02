4ee2ce7a017a73c1046c998ae6e5d0e9540950af
rubencastro24 <rubencastro@uma.es>
2024-05-02 12:11:11 +0200

    primer commit

---
+++ vulnesolve-back/src/main/java/com/uma/vulnesolve/VulnesolveApplication.java
@@ -0,0 +1,13 @@
+package com.uma.vulnesolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnesolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnesolveApplication.class, args);
+	}
+
+}
