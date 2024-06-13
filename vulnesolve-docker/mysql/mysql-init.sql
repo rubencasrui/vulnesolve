b15a35b1825658a7fdf21911ff882fa4a717aa61
rubencastro24 <rubencastro@uma.es>
2024-06-14 00:47:11 +0200

    Update mysql-init.sql

--- vulnesolve-docker/mysql/mysql-init.sql
+++ vulnesolve-docker/mysql/mysql-init.sql
@@ -3,12 +3,3 @@ create database vulnesolve;
 
 use vulnesolve;
 
-drop table if exists producto;
-create table producto(
-	id     int            primary key auto_increment,
-    nombre varchar(255)   not null,
-    marca  varchar(255)   not null,
-    precio decimal(10, 2) not null
-);
-
-
